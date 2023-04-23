de70db433a905a0b15ee1f6522796b4cfd6dbf75
Vinay-ambekar <vinayambekar.b@gmail.com>
2023-04-23 18:15:02 +0530

    commited

---
+++ Event/script.js
@@ -0,0 +1,8 @@
+// Get the button element by its ID
+const button = document.getElementById("myButton");
+
+// Add an event listener to the button element
+button.addEventListener("click", function() {
+  // Execute this code when the button is clicked
+  console.log("Button clicked!");
+});
