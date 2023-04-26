39f4f2a0ed1f4e52213cc2f765c89aa5aa9e37d1
Vinay-ambekar <vinayambekar.b@gmail.com>
2023-04-26 20:03:13 +0530

    commited async & await

--- async_await/script.js
+++ async_await/script.js
@@ -4,18 +4,23 @@ The async and await keywords enable asynchronous,
 promise-based behavior to be written in a cleaner style, 
 avoiding the need to explicitly configure promise chains.
 */
-async function harry() {
+function bng(){
     let bangloreWeather = new Promise((resolve, reject) => {
       setTimeout(() => {
         resolve("Bangalore weather is 27 deg");
       }, 4000);
     });
-  
+    return bangloreWeather
+  }
+
+function cta(){
     let chitradurgaWeather = new Promise((resolve, reject) => {
       setTimeout(() => {
         resolve("Chitradurga weather is 32 deg");
-      }, 4000);
+      }, 2000);
     });
+    return chitradurgaWeather
+}
     /* 
     the below given code .then() is to get resolve from promise but if bot are resolve at same time
     non of them are not going to wait to complete.means it is not going to complete one by one it will
@@ -28,21 +33,24 @@ async function harry() {
     untill the bngw compete its asynchrons task it will not go to another asynchronsu opration,
     after the completion of bngw asnchrons task it will move to  another asynchronsu opration that is ctaw*/
 
-
+    async function harry() {
     console.log("Fetching Bangalore Weather. Please wait....");
-    let bngw = await bangloreWeather;
+    let bngw = await bng()
     console.log("Fetching Bangalore weather is => " + bngw);
     
-    console.log("Fetching Chitradurga Weather. Please wait....");
-    let ctaw = await chitradurgaWeather;
+  console.log("Fetching Chitradurga Weather. Please wait....");
+    let ctaw = await cta()
     console.log("Fetching Chitradurga weather is => " + ctaw);
     
     return [bngw, ctaw];
-  }
+    }
   
-  alert("Welcome to Weather Control Room");
+  console.log("Welcome to Weather Control Room");
   let a = harry();
+
+  //console.log(a) or
   a.then((value) => {
-    console.log(value);
-  });
-    
+    console.log(value)
+  
+  }).catch((err)=>{console.log(err)})
+   
