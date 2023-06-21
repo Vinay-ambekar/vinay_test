d14f0377075ff17ba2e502bb6f0e53db672b147d
Vinay-ambekar <vinayambekar.b@gmail.com>
2023-06-21 08:53:14 +0530

    'string'

---
+++ 06strings/script.js
@@ -0,0 +1,15 @@
+let name01="harry"// inside a double colon harry is string 
+console.log(name01)
+console.log(name01.length)//tell the length of the string
+let a=`heloo i am ${name01}`//here we are using backtiks
+console.log(a)
+let x='Adom D\'Angelo '//out put Adom D'Angelo, \n for new line
+console.log(x)
+console.log(name01.toUpperCase)//print HARRY
+console.log(name01.toLowerCase)//print harry
+console.log(name01.slice(2,4))//it print from 2 to 4 that is rr
+console.log(name01.slice(2))//it print from 2 that is rry
+console.log(x.replace("Adom","vini"))//out put vini D'Angelo 
+let y='      hello'
+console.log(y.trim())//removes strarting white space
+console.log(name01[0])//prints h
