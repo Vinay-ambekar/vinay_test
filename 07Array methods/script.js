b5a785a05959044e183ed0c7c07c6076bdd0d985
Vinay-ambekar <vinayambekar.b@gmail.com>
2023-06-23 07:45:17 +0530

    'commit'

--- 07Array methods/script.js
+++ 07Array methods/script.js
@@ -128,7 +128,6 @@ const squaredNumbers = numbers03.map((number) => {
 });
 console.log(squaredNumbers); // Output: [1, 4, 9, 16, 25]
 console.log(numbers03); // Output: [1, 2, 3, 4, 5]
-
 //2.Modifying an array of objects:
 const students = [
     { name: 'Alice', age: 20 },
@@ -146,5 +145,226 @@ const doubledNumbers = numbers04.map(number => number * 2);
 console.log(doubledNumbers); // Output: [2, 4, 6, 8, 10]
 console.log(numbers04); // Output: [1, 2, 3, 4, 5]
 
+/* filter()
+ In JavaScript, the filter() method is used to create a
+  new array containing 
+ elements from an existing array that meet certain criteria.*/
+//array.filter(callback(element[, index[, array]])[, thisArg])
+
+const numbers05 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const evenNumbers = numbers05.filter(function(number) {
+  return number % 2 === 0;
+});
+console.log(evenNumbers); // Output: [2, 4, 6, 8, 10]
+
+/* reduce
+ In JavaScript, the reduce() method is 
+ used to reduce an array to a single value 
+ by executing a reducer function on each element of the array.*/
+//array.reduce(callback(accumulator, currentValue[, index[, array]])[, initialValue])
+const numbers06 = [1, 2, 3, 4, 5,6];
+const sum = numbers06.reduce(function(accumulator, currentValue) {
+  return accumulator + currentValue;
+});
+console.log(sum); // Output: 15
+
+/* for...of 
+In JavaScript, the for...of loop is used to iterate over
+ iterable objects, including arrays, strings, maps, sets, and 
+ more. It provides a simpler and more 
+concise syntax compared to traditional for loops or forEach() */
+/* 
+for (variable of iterable) {
+  // code to be executed for each iteration
+}
+*/
+//1.Iterating over an Array:
+const numbers07 = [1, 2, 3, 4, 5];
+
+for (const number of numbers07) {
+  console.log(number);
+}
+//2.Iterating over a String:
+const message = "Hello";
+
+for (const char of message) {
+  console.log(char);
+}
+//Iterating over a Map:
+const map = new Map();
+map.set(1, 'one');
+map.set(2, 'two');
+map.set(3, 'three');
+
+for (const [key, value] of map) {
+  console.log(key, value);
+}
+/* for...in
+In JavaScript, the for...in 
+loop is used to iterate over the properties of an object. */
+/* 
+for (variable in object) {
+  // code to be executed for each property
+}
+*/
+const person = {
+  name: 'John',
+  age: 30,
+  city: 'New York'
+};
+for (const key in person) {
+  console.log(key + ': ' + person[key]);
+}
+//hasOwnProperty():
+for (const key in person) {
+  if (person.hasOwnProperty(key)) {
+    console.log(key + ': ' + person[key]);
+  }
+}
+/* forEach()
+  forEach() that allows you to iterate over the elements of
+   an array or the values of a map or set. It provides a 
+   convenient way to perform an operation 
+  on each element/value without the need for manual indexing.*/
+//Iterating over an Array:
+const numbers08= [1, 2, 3, 4, 5];
+
+numbers08.forEach(function(number) {
+  console.log(number);
+});
+//Iterating over a Map:
+const map03 = new Map();
+map.set(1, 'one');
+map.set(2, 'two');
+map.set(3, 'three');
+
+map03.forEach(function(value, key) {
+  console.log(key, value);
+});
+/*o/p
+1 'one'
+2 'two'
+3 'three' 
+ */
+//Iterating over a Set:
+const set = new Set([1, 2, 3, 4, 5]);
+set.forEach(function(value) {
+  console.log(value);
+});
+
+/*  jion
+
+In JavaScript, the join() method is used to create
+ a string by joining the elements of an array together
+*/
+//Joining an Array with Default Separator:
+const fruits03 = ['apple', 'banana', 'orange'];
+const result = fruits03.join();
+console.log(result); // Output: "apple,banana,orange"
+//Joining an Array with Custom Separator:
+const colors01= ['red', 'green', 'blue'];
+const result01 = colors.join('-');
+console.log(result01); // Output: "red-green-blue"
+
+/* shift()
+In JavaScript, the shift() method is 
+used to remove and return the first element from an array */
+const fruits04= ['apple', 'banana', 'orange'];
+const shiftedElement = fruits.shift();
+console.log(shiftedElement); // Output: "apple"
+console.log(fruits04); // Output: ["banana", "orange"]
+
+//Here's an example with an empty array:
+const emptyArray = [];
+const shiftedElement01 = emptyArray.shift();
+console.log(shiftedElement01); // Output: undefined
+console.log(emptyArray); // Output: []
+
+/* 
+unshift()
+In JavaScript, the unshift() method is used 
+to add one or more elements to the beginning of an array.
+*/
+//array.unshift(element1[, element2, ..., elementN])
+const fruits08 = ['banana', 'orange'];
+fruits08.unshift('apple');
+console.log(fruits08); // Output: ["apple", "banana", "orange"]
+
+const numbers09 = [3, 4, 5];
+
+numbers09.unshift(1, 2);
+
+console.log(numbers09); // Output: [1, 2, 3, 4, 5]
+
+/* delet()
+In JavaScript, the delete operator is used to delete a 
+property from an object or to remove an element from an array. 
+*/
+//Deleting a Property from an Object:
+const person02= {
+  name: 'John',
+  age: 30,
+  city: 'New York'
+};
+delete person02.age;
+console.log(person02); // Output: {name: "John", city: "New York"}
+//Deleting an Element from an Array:
+const numbers10 = [1, 2, 3, 4, 5];
+delete numbers10[2];
+console.log(numbers10); // Output: [1, 2, undefined, 4, 5]
+
+/* concat()
+
+In JavaScript, the concat() method is used to merge two or
+ more arrays or combine arrays with additional elements.  */
+//array.concat(value1[, value2, ..., valueN])
+//Concatenating Two Arrays:
+const array1 = [1, 2, 3];
+const array2 = [4, 5, 6];
+const result02 = array1.concat(array2);
+console.log(result02); // Output: [1, 2, 3, 4, 5, 6]
+//Concatenating Array with Additional Values:
+const array03 = [1, 2, 3];
+const result03 = array03.concat(4, 5, 6);
+console.log(result03); // Output: [1, 2, 3, 4, 5, 6]
+//Combining Multiple Arrays:
+const array01 = [1, 2];
+const array02 = [3, 4];
+const array05 = [5, 8];
+const result04 = array01.concat(array02, array05);
+console.log(result04); // Output: [1, 2, 3, 4, 5, 6]
+
+/* reverse()
+In JavaScript, the reverse() method is used
+ to reverse the order of elements in an array
+*/
+const array08= [1, 2, 3, 4, 5];
+array08.reverse();
+console.log(array08); // Output: [5, 4, 3, 2, 1]
+//** revers the string*/
+const str = 'Hello, World!';
+const reversedStr = str.split('').reverse().join('');
+console.log(reversedStr); // Output: '!dlroW ,olleH'
+
+/* Array.from()
+In JavaScript, the Array.from() method is used to create 
+a new array from an iterable object or an array-like object. 
+
+Array.from(iterable[, mapFn[, thisArg]])
+*/
+//Converting a string to an array:
+const str03= 'Hello';
+const arr = Array.from(str03);
+console.log(arr); // Output: ['H', 'e', 'l', 'l', 'o']
+//Converting a Set to an array:
+const set02= new Set([1, 2, 3]);
+const arr02= Array.from(set02);
+console.log(arr02); // Output: [1, 2, 3]
+//Mapping function during conversion:
+const arrayLike = { length: 3, 0: 'a', 1: 'b', 2: 'c' };
+const arr03 = Array.from(arrayLike, function(item) {
+  return item.toUpperCase();
+});
+console.log(arr03); // Output: ['A', 'B', 'C']
 
 
